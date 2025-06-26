a3121171f68a7fe41e22261dc71adcee121041b7
“sbyu” <sbyu@foodtechkorea.com>
2025-06-26 20:35:55 +0900

    refactor: 레이어드 아키텍처 패턴에 맞게 패키지 구조 개선
    - presentation 레이어
      - controller 패키지를 presentation/controller로 이동
      - dto, interceptor, argument-resolver 디렉토리 구조 추가
    
    - application 레이어
      - dto, service 디렉토리 구조 추가
    
    - domain 레이어
      - model, vo 디렉토리 구조 추가
    
    - infrastructure 레이어
      - config 패키지를 infrastructure/config로 이동
      - entity, repository, client 디렉토리 구조 추가

--- booker-server/src/main/java/com/bookerapp/core/presentation/controller/BookController.java
+++ booker-server/src/main/java/com/bookerapp/core/presentation/controller/BookController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.bookerapp.core.presentation.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
