a0a509a6f08ee557b14a65f8af89a2e7bc975a4c
Sebin Yu <sebinyu1137@gmail.com>
2025-07-04 22:26:11 +0900

    chore: BaseEntity 클래스 - 사용 x 주석 삭제

--- booker-server/src/main/java/com/bookerapp/core/domain/model/BaseEntity.java
+++ booker-server/src/main/java/com/bookerapp/core/domain/model/BaseEntity.java
@@ -30,33 +30,18 @@ public abstract class BaseEntity {
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted = false;
 
-    /**
-     * 생성자 정보를 설정합니다.
-     * 주로 인증된 사용자 정보를 기반으로 설정됩니다.
-     */
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
     }
 
-    /**
-     * 수정자 정보를 설정합니다.
-     * 주로 인증된 사용자 정보를 기반으로 설정됩니다.
-     */
     public void setUpdatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
     }
 
-    /**
-     * 소프트 삭제를 수행합니다.
-     * 실제 데이터를 삭제하지 않고 삭제 플래그만 변경합니다.
-     */
     public void markAsDeleted() {
         this.isDeleted = true;
     }
 
-    /**
-     * 삭제를 취소합니다.
-     */
     public void unmarkAsDeleted() {
         this.isDeleted = false;
     }
