bf853be7f854018a908709a3c4dd7d56a3b177d1
Sebin Yu <sebinyu1137@gmail.com>
2025-07-13 21:00:26 +0900

    refactor: Request/Response DTO 통합 구조 반영

--- booker-server/src/main/java/com/bookerapp/core/domain/model/response/UserResponse.java
+++ booker-server/src/main/java/com/bookerapp/core/domain/model/response/UserResponse.java
@@ -1,16 +1,31 @@
 package com.bookerapp.core.domain.model.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import java.util.List;
 import com.bookerapp.core.domain.model.auth.Role;
 
-@Getter
-@RequiredArgsConstructor
 public class UserResponse {
-    private final String userId;
-    private final String username;
-    private final String email;
-    private final List<Role> roles;
-    private final boolean authenticated;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Request {
+        private String userId;
+        private String username;
+        private String email;
+        private List<Role> roles;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public static class Response {
+        private final String userId;
+        private final String username;
+        private final String email;
+        private final List<Role> roles;
+        private final boolean authenticated;
+    }
 } 
