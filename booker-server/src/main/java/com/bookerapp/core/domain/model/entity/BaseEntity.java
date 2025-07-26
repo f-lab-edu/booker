a0ede84cbf90e8b40fae8be953251275dc2e8c34
Sebin Yu <sebinyu1137@gmail.com>
2025-07-26 20:08:03 +0900

    feat: JPA Auditing 기능 추가하여 엔티티 생성자/수정자 자동 관리
    - BaseEntity에 @CreatedBy, @LastModifiedBy 어노테이션 추가
    - JpaAuditingConfig 추가하여 Auditing 설정 구성
    - 수동으로 setUpdatedBy 호출하는 코드 제거
    - 엔티티 생성/수정 시 자동으로 사용자 정보 기록

--- booker-server/src/main/java/com/bookerapp/core/domain/model/entity/BaseEntity.java
+++ booker-server/src/main/java/com/bookerapp/core/domain/model/entity/BaseEntity.java
@@ -2,7 +2,9 @@ package com.bookerapp.core.domain.model.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -21,22 +23,16 @@ public abstract class BaseEntity {
     @Column(nullable = false)
     private LocalDateTime updatedAt;
 
-    @Column(length = 50)
+    @CreatedBy
+    @Column(name = "created_by", length = 100, updatable = false)
     private String createdBy;
 
-    @Column(length = 50)
+    @LastModifiedBy
+    @Column(name = "updated_by", length = 100)
     private String updatedBy;
 
     @Column(nullable = false)
     private boolean isDeleted;
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
 
     public void markAsDeleted() {
         this.isDeleted = true;
