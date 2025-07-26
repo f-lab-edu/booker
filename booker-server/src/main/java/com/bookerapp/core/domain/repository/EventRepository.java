4b0861a8312ccecb4193341d3f93347c84bdaeb8
Sebin Yu <sebinyu1137@gmail.com>
2025-07-26 20:39:24 +0900

    feat: 이벤트 조회 페이지네이션 도입

--- booker-server/src/main/java/com/bookerapp/core/domain/repository/EventRepository.java
+++ booker-server/src/main/java/com/bookerapp/core/domain/repository/EventRepository.java
@@ -2,10 +2,11 @@ package com.bookerapp.core.domain.repository;
 
 import com.bookerapp.core.domain.model.event.Event;
 import com.bookerapp.core.domain.model.event.EventType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface EventRepository extends JpaRepository<Event, Long> {
-    List<Event> findByType(EventType type);
+    Page<Event> findByType(EventType type, Pageable pageable);
+    Page<Event> findAll(Pageable pageable);
 } 
--- booker-server/src/main/java/com/bookerapp/core/presentation/controller/EventController.java
+++ booker-server/src/main/java/com/bookerapp/core/presentation/controller/EventController.java
@@ -6,6 +6,9 @@ import com.bookerapp.core.domain.model.event.EventType;
 import com.bookerapp.core.domain.service.EventService;
 import com.bookerapp.core.presentation.dto.event.EventDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -65,14 +68,12 @@ public class EventController {
 
     @GetMapping
     @RequireRoles({Role.ADMIN, Role.USER})
-    public ResponseEntity<List<EventDto.Response>> getAllEvents(
-            @RequestParam(required = false) EventType type) {
-        List<Event> events = type != null ?
-                eventService.findEventsByType(type) :
-                eventService.findAllEvents();
-        return ResponseEntity.ok(events.stream()
-                .map(EventDto.Response::from)
-                .toList());
+    public ResponseEntity<EventDto.PageResponse> getEvents(
+            @RequestParam(required = true) EventType type,
+            @PageableDefault(size = 20) Pageable pageable) {
+        
+        Page<Event> events = eventService.findEventsByType(type, pageable);
+        return ResponseEntity.ok(EventDto.PageResponse.from(events));
     }
 
     @GetMapping("/{eventId}")
