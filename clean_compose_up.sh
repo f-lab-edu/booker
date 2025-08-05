f60b1f11e548b8426b0d6125ebffdd6627337636
“sbyu” <sbyu@foodtechkorea.com>
2025-08-06 00:48:33 +0900

    feat: Prometheus 모니터링 및 Testcontainers 의존성 추가
    - Spring Boot Actuator, Prometheus 메트릭 수집 추가
    - MySQL-exporter, k6 의존성 추가
    - 환경 설정 파일 및 스크립트 추가

---
+++ clean_compose_up.sh
@@ -0,0 +1,11 @@
+docker-compose stop springboot
+
+docker-compose build --no-cache springboot
+
+docker-compose up -d springboot
+
+docker image prune
+
+docker images | head -5
+
+# docker logs -f springboot
